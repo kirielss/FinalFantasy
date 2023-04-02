b75d61b015e13d1675717840721be34af8ae1765
skiriel <gabrieldllopes@gmail.com>
2023-04-02 10:45:19 -0300

    Add project files.

---
+++ FinalFantasy/FinalFantasyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FinalFantasy.Models;
+
+namespace FinalFantasy
+{
+    public class FinalFantasyDbContext : DbContext
+    {
+        public FinalFantasyDbContext(DbContextOptions<FinalFantasyDbContext> options) : base(options) { }
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<Game> Games { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+    }
+}
+
---
+++ FinalFantasy/Models/Game.cs
@@ -0,0 +1,10 @@
+﻿namespace FinalFantasy.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public virtual ICollection<Character> Characters { get; set; }
+        public string Description { get; set; }
+    }
+}
