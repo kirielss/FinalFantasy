2cf8c9598f5b292510cb01151e97cc70154ab7ba
skiriel <gabrieldllopes@gmail.com>
2023-04-02 15:19:18 -0300

    stuck... cant figure out azure connection

---
+++ FinalFantasy/Data/DbConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace FinalFantasy.Data
+{
+    static public class DbConfig
+    {
+        static private readonly IConfiguration _configuration;
+
+        static DbConfig()
+        {
+            _configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
+        static public string ConnectionString => _configuration.GetConnectionString("DefaultConnection");
+    }
+}
--- FinalFantasy/Program.cs
+++ FinalFantasy/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<FinalFantasyDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlServer(DbConfig.ConnectionString);
 });
 
 var app = builder.Build();
